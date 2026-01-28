a81e760ceed65a94fa69ed8fcedbc563bc849d0d
ph2oconsult-pixeldiffboho <ph2oconsult@gmail.com>
2026-01-28 21:41:15 +1100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useMemo, useEffect } from 'react';
 import { WaterQualityData, SofteningResults, ChartDataPoint } from './types';
 import { calculateSoftening } from './services/softeningCalculations';
